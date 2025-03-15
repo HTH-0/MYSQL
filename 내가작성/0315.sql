902ee1d86b1be44277f2c11192b6bfacea1f5672
HTH-0 <xogud1793@naver.com>
2025-03-15 14:08:08 +0900

    0315

---
+++ 내가작성/0315.sql
@@ -0,0 +1,6 @@
+# INNER JOIN : 두 테이블의 데이터가 일단 전부 나오고, 추가로 ON 구문에 맞는 데이터만 나온다
+# OUTER JOIN (LEFT)
+# LEFT 테이블ㅇㄴ 전부, RIGHT 테이블은 ON 구문에 맞는 데이터만 나온다 (LEFT, RIGHT)
+#( ON 구문이 무조건 필요)
+#NATURAL JOIN : INNER JOIN의 ON 구문을 자동으로 구성해줌
+#CROSS JOIN : 두 테이블의 행 데이터를 서로 한 행씩 결합한 결과가 나온다 ( 의미없는 데이터)
